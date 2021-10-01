4367de5442895f2a4552e4313f498ef6e3ecc2f0
Tjaart van der Walt <git@tjaart.org>
2021-10-01 14:15:30 -0700

    Update lsp config

--- lsp.lua
+++ lsp.lua
@@ -1,15 +1,34 @@
-vim.cmd("call minpac#add('nvim/nvim-lsp')")
 vim.cmd("call minpac#add('nvim/nvim-lspconfig')")
 
+vim.cmd("call minpac#add('kabouzeid/nvim-lspinstall')")
 
-vim.cmd('packadd! nvim-lsp')
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{}
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
 vim.cmd('packadd! nvim-lspconfig')
 
 vim.cmd('set completeopt=menuone,noselect')
 
-vim.api.nvim_set_keymap('n', '[w', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', {})
-vim.api.nvim_set_keymap('n', ']w', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', {})
+vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', {})
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', {})
+
+vim.api.nvim_set_keymap('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
 vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
 vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {})
@@ -18,7 +37,6 @@ vim.api.nvim_set_keymap('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>
 vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {})
 vim.api.nvim_set_keymap('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', {})
 vim.api.nvim_set_keymap('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', {})
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
 
 vim.api.nvim_set_keymap('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>', {})
 vim.api.nvim_set_keymap('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
@@ -37,24 +55,21 @@ local lsp = require 'lspconfig'
 local on_attach = function(client)
 end
 
--- sudo npm install -g vscode-css-languageserver-bin
-lsp.cssls.setup{on_attach = on_attach}
+lsp.css.setup{on_attach = on_attach}
 
--- sudo npm install -g dockerfile-language-server-nodejs
-lsp.dockerls.setup{on_attach = on_attach}
+lsp.dockerfile.setup{on_attach = on_attach}
 
-lsp.gopls.setup{on_attach = on_attach}
+-- lsp.gop.setup{on_attach = on_attach}
 
--- sudo npm install -g vscode-html-languageserver-bin
 lsp.html.setup{on_attach = on_attach}
 
--- sudo npm install -g vscode-json-languageserver
-lsp.jsonls.setup{on_attach = on_attach}
+lsp.json.setup{on_attach = on_attach}
 
-lsp.pyls.setup{on_attach = on_attach}
+lsp.lua.setup{on_attach = on_attach}
 
--- gem install solargraph
-lsp.solargraph.setup{
+lsp.python.setup{on_attach = on_attach}
+
+lsp.ruby.setup{
   on_attach = on_attach,
   settings = {
     solargraph = {
@@ -64,16 +79,11 @@ lsp.solargraph.setup{
   }
 }
 
--- sudo npm install -g typescript typescript-language-server
-lsp.tsserver.setup{on_attach = on_attach}
+lsp.typescript.setup{on_attach = on_attach}
 
--- sudo npm install -g vim-language-server
-lsp.vimls.setup{on_attach = on_attach}
+lsp.vim.setup{on_attach = on_attach}
 
--- sudo npm install -g vls
-lsp.vuels.setup{on_attach = on_attach}
+lsp.vue.setup{on_attach = on_attach}
 
--- sudo npm install -g yaml-language-server
-lsp.yamlls.setup{on_attach = on_attach}
-
+lsp.yaml.setup{on_attach = on_attach}
 
