27ac54b803eb38ef45f5dd2e0c089a3e4baa5716
Tjaart van der Walt <git@tjaart.org>
2023-11-16 10:09:51 -0800

    Remove config that is default in lazy

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,40 +1,46 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.encoding = "UTF-8"
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-vim.opt.list = true
-vim.opt.foldenable = false
-vim.opt.nu = true
-vim.opt.hidden = false
-vim.opt.relativenumber = false
-vim.opt.wrap = true
-vim.opt.confirm = false
 
-vim.opt.omnifunc = "syntaxcomplete#Complete"
-
-vim.opt.showcmd = true
-vim.opt.smartcase = true
-vim.opt.spellfile = "~/.config/nvim/spell/en.utf-8.add"
-vim.opt.spell = true
+vim.g.autoformat = false
+vim.opt.clipboard = "unnamed,unnamedplus"
 vim.opt.expandtab = false
-vim.opt.tags = "tags;/"
-vim.opt.wildmode = "longest:list,full"
+vim.opt.relativenumber = false
 vim.opt.title = true
-vim.opt.titlelen = 33
+vim.opt.wrap = true
+vim.opt.wildmode = "longest:list,full"
 
-vim.opt.termguicolors = true
-vim.opt.updatetime = 750
+-- Maybe keep
+vim.opt.encoding = "UTF-8"
+vim.opt.incsearch = true
+vim.opt.spellfile = "~/.config/nvim/spell/en.utf-8.add"
+
+-- Discard
+-- vim.opt.titlelen = 33
+-- vim.opt.hlsearch = true
+-- vim.opt.ignorecase = true
+-- vim.opt.list = true
+-- vim.opt.foldenable = false
+-- vim.opt.nu = true
+-- vim.opt.hidden = false
+-- vim.opt.confirm = false
+
+-- vim.opt.omnifunc = "syntaxcomplete#Complete"
+
+-- vim.opt.showcmd = true
+-- vim.opt.smartcase = true
+-- vim.opt.spell = true
+-- vim.opt.tags = "tags;/"
+
+-- vim.opt.termguicolors = true
+-- vim.opt.updatetime = 750
 
 -- vim.opt.ch=0
-vim.api.nvim_set_option("clipboard","unnamed,unnamedplus") 
 
-vim.api.nvim_set_option("background","dark") 
+-- vim.api.nvim_set_option("background","dark") 
 
 -- Show only 1 status line
-vim.cmd([[ set laststatus=3 ]])
+-- vim.cmd([[ set laststatus=3 ]])
 
 
 -- "prevent nested instances
@@ -49,9 +55,8 @@ vim.cmd([[ set laststatus=3 ]])
 -- filetype plugin on
 
 -- vim.g['netrw_list_hide'] = '\(^\|\s\s\)\zs\.\S\+'
-vim.g.netrw_keepdir = 0
-vim.g.netrw_sort_options = "i"
-vim.g.autoformat = false
+-- vim.g.netrw_keepdir = 0
+-- vim.g.netrw_sort_options = "i"
 
 -- -- Copy visual selection into system clipboard
 -- -- https://github.com/neovim/neovim/issues/2325
