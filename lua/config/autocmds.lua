bae5744a836cbb0834edfe6d7390085d7b76351e
Tjaart van der Walt <git@tjaart.org>
2024-01-27 20:10:47 -0800

    Remove undofiles autocmd for now

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,16 +1,16 @@
-vim.cmd([[
-"Manage undo files
-set undofile
-"Do not create undo files for /tmp files
-augroup vimrc
-  autocmd!
-  autocmd BufWritePre /tmp/* setlocal noundofile
-augroup END
-
-" set autochdir
-" autocmd BufEnter * silent! lcd %:p:h
-" autocmd TabEnter * silent! tcd %:p:h
-]])
+-- vim.cmd([[
+-- "Manage undo files
+-- set undofile
+-- "Do not create undo files for /tmp files
+-- augroup vimrc
+--   autocmd!
+--   autocmd BufWritePre /tmp/* setlocal noundofile
+-- augroup END
+--
+-- " set autochdir
+-- " autocmd BufEnter * silent! lcd %:p:h
+-- " autocmd TabEnter * silent! tcd %:p:h
+-- ]])
 
 vim.cmd([[
 augroup turbo_commit
