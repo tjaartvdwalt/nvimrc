0f0950fd1eae1889c6edc9146eb694ffdf8423d5
Tjaart van der Walt <git@tjaart.org>
2024-02-16 17:48:04 -0800

    Add firenvim config

--- lua/plugins/editor_local.lua
+++ lua/plugins/editor_local.lua
@@ -118,4 +118,28 @@ return {
 			},
 		},
 	},
+	{
+		"glacambre/firenvim",
+
+		-- Lazy load firenvim
+		-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+		lazy = not vim.g.started_by_firenvim,
+		build = function()
+			vim.fn["firenvim#install"](0)
+		end,
+		init = function()
+			vim.g.firenvim_config = {
+				-- globalSettings = { alt = "all" },
+				localSettings = {
+					[".*"] = {
+						-- cmdline  = "neovim",
+						-- content  = "text",
+						-- priority = 0,
+						-- selector = "textarea",
+						takeover = "never",
+					},
+				},
+			}
+		end,
+	},
 }
