3a8fb80ed2efed1c2cf61ebb4f0103838a2d01de
Tjaart van der Walt <git@tjaart.org>
2024-02-16 17:47:51 -0800

    Add otter as a nvim-cmp source

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,16 +1,22 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      completion = {
-        autocomplete = false,
-      },
-      mapping = require("cmp").mapping.preset.insert({
-        ["<C-Enter>"] = require("cmp").mapping.complete(),
-      }),
-    },
-  },
-  { "echasnovski/mini.pairs", enabled = false },
-  -- { "echasnovski/mini.surround", enabled = false },
-  --{ "echasnovski/mini.comment", enabled = false },
+	{
+		"hrsh7th/nvim-cmp",
+		opts = {
+			completion = {
+				autocomplete = false,
+			},
+			mapping = require("cmp").mapping.preset.insert({
+				["<C-Enter>"] = require("cmp").mapping.complete(),
+			}),
+			sources = require("cmp").config.sources({
+				{ name = "otter" },
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+				{ name = "buffer" },
+			}),
+		},
+	},
+	{ "echasnovski/mini.pairs", enabled = false },
+	-- { "echasnovski/mini.surround", enabled = false },
+	--{ "echasnovski/mini.comment", enabled = false },
 }
